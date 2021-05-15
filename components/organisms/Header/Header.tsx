22d19ac0b7a91f4d79f5de9adf0a82d3062233e2
Marion <katyanne_mm@live.fr>
2021-05-15 17:21:19 +0200

    Modify Header

--- components/organisms/Header/Header.tsx
+++ components/organisms/Header/Header.tsx
@@ -1,4 +1,10 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu , } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import React from 'react';
 
 const { Header: AntdHeader } = Layout;
@@ -6,11 +12,14 @@ const { Header: AntdHeader } = Layout;
 export default function Header() {
   return (
     <AntdHeader>
+      <Router>
       <Menu theme="dark" mode="horizontal">
-        <Menu.Item key="nav-home">Home</Menu.Item>
-        <Menu.Item key="nav-languages">Languages</Menu.Item>
-        <Menu.Item key="nav-strategies">Strategies</Menu.Item>
+        <Menu.Item key="nav-home"><Link to="/">Home</Link></Menu.Item>
+        <Menu.Item key="nav-languages"><Link to="/languages">Languages</Link></Menu.Item>
+        <Menu.Item key="nav-strategies"><Link to="/strategies">Strategies</Link></Menu.Item>
+        <Menu.Item key="nav-login"><Link to="/login">Login</Link></Menu.Item>
       </Menu>
+      </Router>
     </AntdHeader>
   );
 }
