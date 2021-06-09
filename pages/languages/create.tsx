ef115065064d5084885b4ffad544cb6ac2c67ea6
quantumsheep <nathanael.dmc@outlook.fr>
2021-06-09 13:16:00 +0200

    Implement strategy execution

--- pages/languages/create.tsx
+++ pages/languages/create.tsx
@@ -1,6 +1,6 @@
-import { Button, Input, notification } from 'antd';
+import { Button, Input, notification, Row } from 'antd';
 import Router from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import { FormEditor } from '../../components/atoms/Editor/Editor';
 import { SingleFileFormEditor } from '../../components/atoms/Editor/SingleFileEditor';
 import Form from '../../components/atoms/Form/Form';
@@ -8,11 +8,16 @@ import FormItem from '../../components/atoms/FormItem/FormItem';
 import Page from '../../components/templates/Page/Page';
 import * as api from '../../utils/api';
 import { LanguageDTO } from '../../utils/dto/language.dto';
-import styles from './create.module.scss';
 
 export default function CreateLanguagePage() {
+  const [isCreateLoading, setIsCreateLoading] = useState(false);
+
   const onCreate = async (language: Omit<LanguageDTO, '_id'>) => {
+    setIsCreateLoading(true);
+
     const { data, error } = await api.client.createLanguage(language);
+
+    setIsCreateLoading(false);
 
     if (error) {
       notification.error({
@@ -75,14 +80,14 @@ export default function CreateLanguagePage() {
         <FormItem label="Files" name="files" wrapperCol={{ span: 24 }}>
           <FormEditor height="800px" />
         </FormItem>
-        <FormItem
-          wrapperCol={{ span: 24 }}
-          className={styles['submit-button-wrapper']}
-        >
-          <Button type="primary" htmlType="submit">
-            Create
-          </Button>
-        </FormItem>
+
+        <Row justify="end">
+          <FormItem>
+            <Button type="primary" loading={isCreateLoading} htmlType="submit">
+              Create
+            </Button>
+          </FormItem>
+        </Row>
       </Form>
     </Page>
   );
