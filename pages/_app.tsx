7c010ae78ab771bed5ba7ed86cae62741f36acde
quantumsheep <nathanael.dmc@outlook.fr>
2021-06-12 00:11:43 +0200

    Improve error handling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,19 @@ import 'semantic-ui-css/semantic.min.css';
 import 'antd/dist/antd.css';
 import '../styles/globals.scss';
 import { hasErrorProps } from '../utils/maybe-error-props';
+import Page from '../components/templates/Page/Page';
+import { UserDTO } from '../utils/dto/user.dto';
 
 export default function App({ Component, pageProps }: AppProps) {
   if (hasErrorProps(pageProps)) {
     return (
-      <Error
-        statusCode={pageProps.error.status}
-        title={pageProps.error.message}
-      />
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      <Page currentUser={pageProps.user as UserDTO} title="Error" subTitle="">
+        <Error
+          statusCode={pageProps.error.status}
+          title={pageProps.error.message}
+        />
+      </Page>
     );
   }
 
--- utils/maybe-error-props.ts
+++ utils/maybe-error-props.ts
@@ -9,6 +9,7 @@ export type MaybeErrorProps<T = Record<string, never>> = Readonly<
 
 export function hasErrorProps<T>(props: MaybeErrorProps<T>): props is Readonly<{
   error: ApiErrorBase;
+  [key: string]: unknown;
 }> {
   return !!(props as never)['error'];
 }
